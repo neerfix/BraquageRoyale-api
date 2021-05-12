c21385692e54df9c44a51947be494c75ef8f0ebd
Nicolas Notararigo (Neerfix) <nicolas.notararigo@ynov.com>
2021-05-12 15:54:40 +0200

    test deploy

--- functions/src/Controller/UsersController.js
+++ functions/src/Controller/UsersController.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const userService = require("../../Services/Users/users.services");
-const Http_response = require('../../Utils/http-response');
 
 // routes -> /users/
 
