9785c8f915c69d8f488a357072e8b9fc5b7afae0
Nicolas Notararigo (Neerfix) <nicolas.notararigo@ynov.com>
2021-06-04 15:51:56 +0200

    fix: return error

--- functions/src/Controller/ObjectsController.js
+++ functions/src/Controller/ObjectsController.js
@@ -1,5 +1,6 @@
 const objectService = require("../Service/ObjectsService");
 const objectRepository = require("../Repository/ObjectsRepository");
+const Http_response = require('../Utils/http-response');
 
 // routes -> /objects/
 module.exports = {
@@ -14,33 +15,48 @@ function getAllObjects (req, res) {
     objectRepository
         .getAllObjects(req, res)
         .then((maps) => res.send(maps))
-        .catch(err => console.error(err));
+        .catch(err => {
+            console.error(err)
+            Http_response.HTTP_500(req, res, '', err)
+        });
 }
 
 function getOneObject (req, res) {
     objectRepository
         .getOneObjectById(req, res)
         .then((objects) => res.send(objects))
-        .catch(err => console.error(err));
+        .catch(err => {
+            console.error(err)
+            Http_response.HTTP_500(req, res, '', err)
+        });
 }
 
 function createNewObject (req, res) {
     objectService
         .createNewObject(req, res)
         .then((objects) => res.send(objects))
-        .catch(err => console.error(err));
+        .catch(err => {
+            console.error(err)
+            Http_response.HTTP_500(req, res, '', err)
+        });
 }
 
 function updateObjectById (req, res) {
     objectService
         .updateObjectById(req, res)
         .then((objects) => res.send(objects))
-        .catch(err => console.error(err));
+        .catch(err => {
+            console.error(err)
+            Http_response.HTTP_500(req, res, '', err)
+        });
 }
 
 function deleteObjectById (req, res) {
     objectService
         .deleteObjectById(req, res)
         .then((objects) => res.send(objects))
-        .catch(err => console.error(err));
+        .catch(err => {
+            console.error(err)
+            Http_response.HTTP_500(req, res, '', err)
+        });
 }
