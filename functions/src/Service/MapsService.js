a7d3036666fbc822840a6d43de74821b73d119a1
Antoine Mousset <amousset@secuserve.com>
2021-06-16 21:27:47 +0200

    feat: updating the maps services

--- functions/src/Service/MapsService.js
+++ functions/src/Service/MapsService.js
@@ -4,6 +4,7 @@ const firebase = require('../Utils/firebase');
 const C = require("../Utils/Constant");
 const uuid = require('uuid');
 const admin = require('firebase-admin');
+const requireCheck = require('../Utils/RequireCheck');
 const Http_response = require("../Utils/http-response");
 
 module.exports = {
@@ -33,7 +34,24 @@ async function createNewMap(req, res) {
 }
 
 async function updateMapById(req, res) {
-    Http_response.HTTP_200(req, res, '', 'hello')
+    requireCheck.check(req, res, req.body.name, 'name', 'string')
+    requireCheck.check(req, res, req.body.files_url, 'files_url', 'string')
+
+    const body = {
+        date: {
+            created_at: new Date(),
+            updated_at: new Date(),
+        },
+        name: req.body.name,
+        description: req.body.description,
+        status: C.STATUS_ACTIVE,
+        vote: req.body.vote,
+        version: {
+            version_number: req.body.version_number ,
+            files_url: req.body.files_url,
+        }
+    }
+    await firebase.update(req, res, 'maps', body, req.params.mapId)
 }
 
 async function deleteMapById(req, res) {
