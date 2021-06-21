772fc99a715556ad5d92d34ac18a8c0ce996aeac
Nicolas Notararigo (Neerfix) <nicolas.notararigo@ynov.com>
2021-06-21 16:44:54 +0200

    feat: add route to create turn of player

--- functions/src/Controller/Games/ActionsGamesController.js
+++ functions/src/Controller/Games/ActionsGamesController.js
@@ -1,15 +1,25 @@
 const actionGameService = require("../../Service/Games/ActionsGamesService");
-const gameRepository = require("../../Repository/GamesRepository");
 const Http_response = require('../../Utils/http-response');
 
 // routes -> /games/
 module.exports = {
     start,
+    update
 }
 
 function start (req, res) {
     actionGameService
         .start(req, res)
+        .then((games) => res.send(games))
+        .catch(err => {
+            console.error(err)
+            Http_response.HTTP_500(req, res, '', err)
+        });
+}
+
+function update (req, res) {
+    actionGameService
+        .update(req, res)
         .then((games) => res.send(games))
         .catch(err => {
             console.error(err)
